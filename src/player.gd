54af6aff802387e626f9703ad18c45f64a49e9b6
Mélo <melodiekouy@gmail.com>
2024-11-09 13:06:52 -0500

    réorganisation fichiers

--- src/player.gd
+++ src/player.gd
@@ -1,31 +1,37 @@
 extends CharacterBody2D
-@export var speed = 400
-var screen_size
 
+@export var speed = 400
+
+@onready var actionable_finder: Area2D = $Direction/ActionableFinder
+
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var vel = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		vel.x += 1
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		vel.x -= 1
 	if Input.is_action_pressed("jump"):
-		velocity.y -= 1
+		vel.y -= 1
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		vel.y += 1
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	if vel.length() > 0:
+		vel = vel.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
 	
-	var collision = move_and_collide(velocity * delta)
+	var collision = move_and_collide(vel * delta)
 	if collision :
-		velocity = velocity.slide(collision.get_normal())
+		vel = vel.slide(collision.get_normal())
+	
+	var actionables = actionable_finder.get_overlapping_areas()
+	if actionables.size() > 0:
+		actionables[0].action()
