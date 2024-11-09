1b49c660081b8846fdd318816de494a95fe8feb5
MMeche <matthieu.mechineau@outlook.com>
2024-11-09 15:14:53 -0500

    Test sur les anim

--- platform_player.gd
+++ platform_player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 var speed = 300.0
 var jump_speed = -400.0
 
+signal is_running(x :float)
+signal is_jumping
+signal is_airborn
+signal is_grounded
+
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -14,6 +19,7 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
+		is_jumping.emit()
 
 	# Get the input direction.
 	var direction_left = Input.is_action_pressed("move_left")
@@ -21,3 +27,11 @@ func _physics_process(delta):
 	velocity.x = (int(direction_left)*-1 +int(direction_right)*1) * speed
 
 	move_and_slide()
+	
+func _process(delta):
+	if abs(velocity.x) > 0 && is_on_floor():
+		is_running.emit(velocity.x)
+	if !is_on_floor():
+		is_airborn.emit()
+	else :
+		is_grounded.emit()
