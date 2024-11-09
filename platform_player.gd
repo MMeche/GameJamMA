a489d3f1d7a0ced794b3479345d6c07c35b20057
MMeche <matthieu.mechineau@outlook.com>
2024-11-09 12:20:56 -0500

    Fonctionnal Player Movement

---
+++ platform_player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed = 300.0
+var jump_speed = -400.0
+
+# Get the gravity from the project settings so you can sync with rigid body nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+
+	# Get the input direction.
+	var direction_left = Input.is_action_pressed("move_left")
+	var direction_right = Input.is_action_pressed("move_right")
+	velocity.x = (int(direction_left)*-1 +int(direction_right)*1) * speed
+
+	move_and_slide()
