98b408741147819bb68abe883376b6286cba69b2
MMeche <matthieu.mechineau@outlook.com>
2024-11-09 15:37:14 -0500

    Reset

--- src/platform_player.gd
+++ src/platform_player.gd
@@ -1,16 +1,12 @@
 extends CharacterBody2D
 
+@onready var actionable_finder: Area2D = $Direction/ActionableFinder
+
 var speed = 300.0
 var jump_speed = -400.0
 
-signal is_running(x :float)
-signal is_jumping
-signal is_airborn
-signal is_grounded
-
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,7 +15,6 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
-		is_jumping.emit()
 
 	# Get the input direction.
 	var direction_left = Input.is_action_pressed("move_left")
@@ -27,9 +22,14 @@ func _physics_process(delta):
 	velocity.x = (int(direction_left)*-1 +int(direction_right)*1) * speed
 
 	move_and_slide()
-	
-func _process(delta):
-	if abs(velocity.x) > 0 && is_on_floor():
+
+func _process(delta: float) -> void:
+		# Detection interaction
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0 and Input.is_action_just_pressed("Actionable"):
+			# TODO afficher le bouton entrer sur l'écran ?
+			actionables[0].action()
+		if abs(velocity.x) > 0 && is_on_floor():
 		is_running.emit(velocity.x)
 	if !is_on_floor():
 		is_airborn.emit()
