ff2319eade3f187d84f36b9dcc8221f35c782423
Mélo <melodiekouy@gmail.com>
2024-11-10 13:08:46 -0500

    interaction joueur-musée

--- dialogue/museum.gd
+++ dialogue/museum.gd
@@ -8,7 +8,7 @@ var money_given: int = 0
 var invest_mode: bool = false
 
 func invest():
-	var result = money_given * randi() % 20
+	var result = money_given * randi() % 10
 	if randi() % 2: # == 1
 		result *= -1
 	museum_money += result + money_given
@@ -16,18 +16,15 @@ func invest():
 	return result
 	
 func update(money):
-	if invest_mode && true: # TODO vérifier l'argent du joueur
+	if invest_mode && PlayerStats.player_money >= money:
 		money_given += money
-#		TODO retir sous joueur
+		PlayerStats.player_money -= money
 	elif !invest_mode && money_given + museum_money > 0 :
 		money_given -= money
-#		TODO donner joueur
+		PlayerStats.player_money += money
 	else:
 		return true
 	return false
 
 # TODO
-#	1. que souhaitez vous faire ?
-#		- ajouter
-#		- retirer
-#		- saisir argent
+#	- saisir argent
