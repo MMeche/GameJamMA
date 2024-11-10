3fce1cf08474fd179341b113cfa1946c3969b0c8
Mélo <melodiekouy@gmail.com>
2024-11-09 20:44:33 -0500

    ennemi qui vole ???

--- src/ennemi.gd
+++ src/ennemi.gd
@@ -1,24 +1,15 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-var speed = 25 # TODO à changer ?
+var speed = 25
 var player_chase = false
 var player = null
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _physics_process(delta: float) -> void:
 	if player_chase:
 		position += (player.position - position) / speed
 
 func _on_detection_arena_body_entered(body: Node2D) -> void:
-	player = body # TODO nécessaire ?
+	player = body
 	player_chase = true
 
 func _on_detection_arena_body_exited(body: Node2D) -> void:
